1a859779993bd13050dc9d349eaab2a1cf6f1194
Sapphire Mason-Brown <sapphire.mason-brown@engineerbetter.com>
2023-01-20 12:25:45 +0000

    run go build script from within yml2env repo directory

--- ci/tasks/release/run.sh
+++ ci/tasks/release/run.sh
@@ -2,14 +2,17 @@
 set -xe
 
 export GOPATH=$PWD/gopath
+export build_dir=$PWD/build
+
+cd yml2env
 
 echo "Building 64-bit Darwin"
-GOARCH=amd64 GOOS=darwin go build -o build/yml2env_osx github.com/EngineerBetter/yml2env
+GOARCH=amd64 GOOS=darwin go build -o $build_dir/yml2env_osx github.com/EngineerBetter/yml2env
 echo "Building 32-bit Linux"
-GOARCH=386 GOOS=linux go build -ldflags '-extldflags "-static"' -o build/yml2env_linux_i686 github.com/EngineerBetter/yml2env
+GOARCH=386 GOOS=linux go build -ldflags '-extldflags "-static"' -o $build_dir/yml2env_linux_i686 github.com/EngineerBetter/yml2env
 echo "Building 64-bit Linux"
-GOARCH=amd64 GOOS=linux go build -ldflags '-extldflags "-static"' -o build/yml2env_linux_x86-64 github.com/EngineerBetter/yml2env
+GOARCH=amd64 GOOS=linux go build -ldflags '-extldflags "-static"' -o $build_dir/yml2env_linux_x86-64 github.com/EngineerBetter/yml2env
 echo "Building 32-bit Windows"
-GOARCH=386 GOOS=windows go build -o build/yml2env_win32.exe github.com/EngineerBetter/yml2env
+GOARCH=386 GOOS=windows go build -o $build_dir/yml2env_win32.exe github.com/EngineerBetter/yml2env
 echo "Building 64-bit Windows"
 GOARCH=amd64 GOOS=windows go build -o build/yml2env_winx64.exe github.com/EngineerBetter/yml2env
